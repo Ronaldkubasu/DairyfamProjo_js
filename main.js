23b4e17cc0c444925a4f9979ffdbee017aec459b
Ronaldkubasu <ronald.kubasu@student.moringashool.com>
2022-07-17 17:59:56 +0300

    added code

---
+++ main.js
@@ -0,0 +1,16 @@
+const TOTAL_PRODUCTION = [
+    {
+      month: "January",
+      volume: 1000,
+    },
+    {
+      month: "February",
+      volume: 1500,
+    },
+
+    {
+        month: "April",
+        volume: 700,
+    }
+  ];
+  
