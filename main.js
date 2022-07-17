7d5b55adc23994c888c1e3294c1a1d14d469b410
Ronaldkubasu <ronald.kubasu@student.moringashool.com>
2022-07-17 18:13:11 +0300

    added code

--- main.js
+++ main.js
@@ -14,3 +14,26 @@ const TOTAL_PRODUCTION = [
     }
   ];
   
+  const tableBodyEl = document.querySelector("#data-table tbody");
+  const resultsEl = document.querySelector("#results");
+  const priceInput = document.querySelector("#price");
+  
+  function displayTotalProduction() {
+    // Study DOM manipulation in JavaScript
+    for (const [index, production] of TOTAL_PRODUCTION.entries()) {
+      const row = document.createElement("tr");
+      const idCol = document.createElement("td");
+      const monthCol = document.createElement("td");
+      const volumeCol = document.createElement("td");
+  
+      idCol.textContent = index;
+      monthCol.textContent = production.month;
+      volumeCol.textContent = production.volume;
+  
+      row.appendChild(idCol);
+      row.appendChild(monthCol);
+      row.appendChild(volumeCol);
+      tableBodyEl.appendChild(row);
+    }
+  }
+  
